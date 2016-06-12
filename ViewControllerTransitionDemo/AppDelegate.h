642a7ab3f1ece4753e8a43c96b184ed8bc616b49
正权 徐 <xuzhengquan@yundianjia.com>
2016-06-12 14:33:12 +0800

    临时提交

---
+++ ViewControllerTransitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewControllerTransitionDemo
+//
+//  Created by 权仔 on 16/6/12.
+//  Copyright © 2016年 XZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewControllerTransitionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ViewControllerTransitionDemo
+//
+//  Created by 权仔 on 16/6/12.
+//  Copyright © 2016年 XZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
